9f8c77f8cd2b2704eac939f82f67f798c73df15d
Samuel Beaulieu <Samuel.beaulieu28@gmail.com>
2017-05-06 16:48:27 -0400

    [FIX] Multilanguage should now be fixed.

--- Unity/Farm Clicker/Assets/Scripts/PlayerCustomization.cs
+++ Unity/Farm Clicker/Assets/Scripts/PlayerCustomization.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerCustomization : MonoBehaviour {
-
+//git
 	public GameObject btnBoy;
 	public GameObject btnGirl;
 	public GameObject btnNextHat;
