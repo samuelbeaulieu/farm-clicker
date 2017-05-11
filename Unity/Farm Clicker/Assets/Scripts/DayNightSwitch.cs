1ec6ae233233907e276768800e5fc8ce437648c7
Samuel Beaulieu <Samuel.beaulieu28@gmail.com>
2017-05-10 21:13:07 -0400

    [FEATURE] Day & Night part one [FIX] Some bug fixes

---
+++ Unity/Farm Clicker/Assets/Scripts/DayNightSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightSwitch : MonoBehaviour {
+
+	public GameObject imgDay;
+	public GameObject imgNight;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("SwitchToDay", 0f);
+	}
+
+	void SwitchToNight () {
+		LeanTween.rotate (imgNight.GetComponent<RectTransform>(), 360f, 10f);
+		LeanTween.alpha(imgDay, 1f, 5f).setDelay(5f);
+		imgDay.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,0f);
+		Invoke ("SwitchToDay", 10f);
+	}
+
+	void SwitchToDay () {
+		LeanTween.rotate (imgDay.GetComponent<RectTransform>(), 360f, 10f);
+		LeanTween.alpha(imgNight, 1f, 5f).setDelay(5f);
+		imgNight.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,1f);
+		Invoke ("SwitchToNight", 10f);
+	}
+
+	void ResetDay () {
+		LeanTween.alpha(imgDay, 1f, 0f);
+		imgDay.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,1f);
+		Invoke ("SwitchToDay", 0f);
+	}
+
+	void ResetNight () {
+		LeanTween.alpha(imgDay, 1f, 0f);
+		imgDay.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,1f);
+		Invoke ("SwitchToNight", 0f);
+	}
+}
